9ac9eebe7a706372fc3807fb109388f654ba77dd
MaDemartini <ma.demartini@duocuc.com>
2024-11-27 18:10:28 -0300

    cambio de url

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ document.addEventListener("DOMContentLoaded", () => {
         // Mostrar botón de descarga
         content.innerHTML = `
             <button id="download-button">Descargar PYMEsGPS</button>
-            <p>O copia este enlace en tu navegador: <a href="https://www.mediafire.com/file/dy8moxo7lsohh45/PYMESGPS.apk/file" target="_blank">Descargar PYMEsGPS</a></p>
+            <p>O copia este enlace en tu navegador: <a href="https://www.mediafire.com/file/9w79nte46foxsyd/PYMESGPS.apk/file" target="_blank">Descargar PYMEsGPS</a></p>
         `;
 
         // Evento del botón
         document
             .getElementById("download-button")
             .addEventListener("click", () => {
-                window.location.href = "https://www.mediafire.com/file/dy8moxo7lsohh45/PYMESGPS.apk/file";
+                window.location.href = "https://www.mediafire.com/file/9w79nte46foxsyd/PYMESGPS.apk/file";
             });
     } else {
         // Mostrar QR
         content.innerHTML = `
             <p>Escanea el siguiente código QR para descargar PYMEsGPS:</p>
-            <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://www.mediafire.com/file/dy8moxo7lsohh45/PYMESGPS.apk/file" alt="Código QR">
-            <p>O copia este enlace en tu navegador: <a href="https://www.mediafire.com/file/dy8moxo7lsohh45/PYMESGPS.apk/file" target="_blank">Descargar PYMEsGPS</a></p>
+            <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://www.mediafire.com/file/9w79nte46foxsyd/PYMESGPS.apk/file" alt="Código QR">
+            <p>O copia este enlace en tu navegador: <a href="https://www.mediafire.com/file/9w79nte46foxsyd/PYMESGPS.apk/file" target="_blank">Descargar PYMEsGPS</a></p>
         `;
     }
 
